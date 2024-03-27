45a603fc663d8e91befe34ad7faadbbc1a2c0bdd
joshmansky <113480306+joshmansky@users.noreply.github.com>
2024-03-26 21:08:32 -0400

    Delete dd_gazebo_ws/build directory
    Deleting as build directory is specific to one's own local machine and is created automatically by colcon when building.

--- dd_gazebo_ws/build/dd_gazebo/CMakeFiles/dd_gazebo_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/dd_gazebo_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/dd_gazebo_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
