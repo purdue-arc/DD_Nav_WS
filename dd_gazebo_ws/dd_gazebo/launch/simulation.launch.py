36af49f60be9c87f96a17eae7e5a1ae4807096d0
BoilerRobo <rover@purdue.edu>
2024-04-10 20:37:38 -0400

    added px4 offboard control and configured the gazebo simulation to be in the ksql airport and with a hexacopter model.

--- dd_gazebo_ws/dd_gazebo/launch/simulation.launch.py
+++ dd_gazebo_ws/dd_gazebo/launch/simulation.launch.py
@@ -1,68 +1,21 @@
-# import launch
-# from launch.substitutions import Command, LaunchConfiguration
-# import launch_ros
-# import os
-
-# def generate_launch_description():
-#     pkg_share = launch_ros.substitutions.FindPackageShare(package='dd_gazebo').find('dd_gazebo')
-#     default_model_path = os.path.join(pkg_share, 'src/description/drone_description.urdf')
-#     default_rviz_config_path = os.path.join(pkg_share, 'rviz/drone_sim_config.rviz')
-
-#     robot_state_publisher_node = launch_ros.actions.Node(
-#         package='robot_state_publisher',
-#         executable='robot_state_publisher',
-#         parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}]
-#     )
-#     joint_state_publisher_node = launch_ros.actions.Node(
-#         package='joint_state_publisher',
-#         executable='joint_state_publisher',
-#         name='joint_state_publisher',
-#         arguments=[default_model_path],
-#         condition=launch.conditions.UnlessCondition(LaunchConfiguration('gui'))
-#     )
-#     joint_state_publisher_gui_node = launch_ros.actions.Node(
-#         package='joint_state_publisher_gui',
-#         executable='joint_state_publisher_gui',
-#         name='joint_state_publisher_gui',
-#         condition=launch.conditions.IfCondition(LaunchConfiguration('gui'))
-#     )
-#     rviz_node = launch_ros.actions.Node(
-#         package='rviz2',
-#         executable='rviz2',
-#         name='rviz2',
-#         output='screen',
-#         arguments=['-d', LaunchConfiguration('rvizconfig')],
-#     )
-
-#     return launch.LaunchDescription([
-#         launch.actions.DeclareLaunchArgument(name='gui', default_value='True',
-#                                             description='Flag to enable joint_state_publisher_gui'),
-#         launch.actions.DeclareLaunchArgument(name='model', default_value=default_model_path,
-#                                             description='Absolute path to robot urdf file'),
-#         launch.actions.DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
-#                                             description='Absolute path to rviz config file'),
-#         joint_state_publisher_node,
-#         joint_state_publisher_gui_node,
-#         robot_state_publisher_node,
-#         rviz_node
-#     ])
-
 import launch
 from launch.substitutions import Command, LaunchConfiguration
+import launch_ros.descriptions
 import launch_ros
 import os
 
 def generate_launch_description():
     pkg_share = launch_ros.substitutions.FindPackageShare(package='dd_gazebo').find('dd_gazebo')
-    default_model_path = os.path.join(pkg_share, 'src/description/drone_description.urdf')
+    default_model_path = os.path.join(pkg_share, 'src/description/urdf/Kwad.xacro')
     default_rviz_config_path = os.path.join(pkg_share, 'rviz/drone_sim_config.rviz')
-    world_path=os.path.join(pkg_share, 'worlds/simple_world.sdf')
+    world_path=os.path.join(pkg_share, 'worlds/ksql_airport.world')
 
     robot_state_publisher_node = launch_ros.actions.Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}]
+        parameters=[{'robot_description': launch_ros.descriptions.ParameterValue(Command(['xacro ', LaunchConfiguration('model')]), value_type=str)}]
     )
+    
     joint_state_publisher_node = launch_ros.actions.Node(
         package='joint_state_publisher',
         executable='joint_state_publisher',
