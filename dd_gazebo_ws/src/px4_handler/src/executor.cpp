da2eec903eaa574ef8412e2f580ff3676610ca36
Millan Kumar <48894200+LelsersLasers@users.noreply.github.com>
2024-10-20 21:51:42 +0000

    Trying to make ModeBase class

--- dd_gazebo_ws/src/px4_handler/src/executor.cpp
+++ dd_gazebo_ws/src/px4_handler/src/executor.cpp
@@ -1,13 +1,51 @@
 #include "rclcpp/rclcpp.hpp"
-#include "px4_ros2_cpp/mode_executor_base.hpp"
-#include "px4_ros2_cpp/mode_base.hpp"
+#include "px4_ros2/components/mode_executor.hpp"
+#include "px4_ros2/components/mode.hpp"
 #include "std_msgs/msg/string.hpp"
+
+class SimpleModeBase : public px4_ros2::ModeBase {
+public:
+    explicit CustomMode(rclcpp::Node & node)
+    : ModeBase(node, Settings{"Custom Mode"}) {
+        // Initialize manual control and rates setpoint
+        // _manual_control_input = std::make_shared<px4_ros2::ManualControlInput>(*this);
+        // _rates_setpoint = std::make_shared<px4_ros2::RatesSetpointType>(*this);
+    }
+
+    void onActivate() override {
+        // Logic when mode is activated
+        // RCLCPP_INFO(_node.get_logger(), "CustomMode activated");
+    }
+
+    void onDeactivate() override {
+        // Logic when mode is deactivated
+        // RCLCPP_INFO(_node.get_logger(), "CustomMode deactivated");
+    }
+
+    void updateSetpoint(const rclcpp::Duration & dt) override {
+        // RCLCPP_INFO(_node.get_logger(), "CustomMode update setpoint");
+        // Update thrust and rates setpoints based on manual control input
+        // Eigen::Vector3f thrust_sp{0.0F, 0.0F, -_manual_control_input->throttle()};
+        // Eigen::Vector3f rates_sp{
+        //     _manual_control_input->roll() * 150.0F * M_PI / 180.0F,
+        //     -_manual_control_input->pitch() * 150.0F * M_PI / 180.0F,
+        //     _manual_control_input->yaw() * 100.0F * M_PI / 180.0F
+        // };
+
+        // _rates_setpoint->update(rates_sp, thrust_sp);
+    }
+
+private:
+    // std::shared_ptr<px4_ros2::ManualControlInput> _manual_control_input;
+    // std::shared_ptr<px4_ros2::RatesSetpointType> _rates_setpoint;
+};
 
 class DroneStateManager : public px4_ros2::ModeExecutorBase {
 public:
-    DroneStateManager(rclcpp::Node & node)
-    : ModeExecutorBase(node, px4_ros2::ModeExecutorBase::Settings{}),
-      _node(node)
+    // Constructor
+    DroneStateManager(rclcpp::Node & node, px4_ros2::ModeBase & owned_mode)
+    : ModeExecutorBase(node, px4_ros2::ModeExecutorBase::Settings{}, owned_mode, "drone_state_manager"),
+      _node(node)  // Initialize _node in the initializer list
     {
         // Subscribe to the "signal" topic
         _signal_subscription = _node.create_subscription<std_msgs::msg::String>(
@@ -18,6 +56,7 @@ public:
     }
 
     enum class State {
+        Reset,
         Idle,
         TakingOff,
         Hovering,
@@ -30,7 +69,7 @@ public:
         RCLCPP_INFO(_node.get_logger(), "DroneStateManager node activated");
     }
 
-    void onDeactivate(DeactivateReason reason) override {
+    void onDeactivate(DeactivateReason _reason) override {
         // Node deactivation logic
         RCLCPP_INFO(_node.get_logger(), "DroneStateManager node deactivated");
     }
@@ -101,3 +140,28 @@ private:
     rclcpp::Node & _node;
     rclcpp::Subscription<std_msgs::msg::String>::SharedPtr _signal_subscription;
 };
+
+
+int main(int argc, char ** argv) {
+    // Initialize the ROS 2 system
+    rclcpp::init(argc, argv);
+
+    // Create an instance of the rclcpp::Node
+    auto node = std::make_shared<rclcpp::Node>("drone_state_manager_node");
+
+    // Create a SimpleModeBase object (required by DroneStateManager)
+    auto mode = std::make_shared<px4_ros2::SimpleModeBase>();
+
+    // Instantiate DroneStateManager with the node and mode objects
+    auto drone_state_manager = std::make_shared<DroneStateManager>(*node, *mode);
+
+    // Activate the DroneStateManager node
+    drone_state_manager->onActivate();
+
+    // Spin the node (this will block and handle callbacks)
+    rclcpp::spin(node);
+
+    // Shutdown ROS 2 system once the spinning is done
+    rclcpp::shutdown();
+    return 0;
+}
